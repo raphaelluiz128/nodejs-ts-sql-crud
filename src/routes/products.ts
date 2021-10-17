80170c80550b8902c32c77492f477a1c252278b7
RAPHAEL LUIZ SCHETTINO <raphaelluiz128@hotmail.com>
2021-10-17 14:32:10 -0300

    Create products.ts

---
+++ src/routes/products.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { productController } from '../controllers/products';
+
+const productRouter = Router();
+productRouter.get('/', productController.listProducts);
+productRouter.get('/:id', productController.getProduct);
+productRouter.post('/', productController.insertProduct);
+productRouter.put('/:id', productController.updateProduct);
+productRouter.delete('/:id', productController.deleteProduct);
+
+export { 
+    productRouter,
+}
